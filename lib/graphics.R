21e4e61782f5198178884ce2c0910f84f39bb7f9
Andrew Heiss <andrew@andrewheiss.com>
2018-03-13 14:22:05 -0600

    Movec general settings out

---
+++ lib/graphics.R
@@ -0,0 +1,36 @@
+# Color palette from ColorBrewer, diverging spectral, 5 categories
+# http://colorbrewer2.org/#type=diverging&scheme=Spectral&n=3
+#
+# library(RColorBrewer)
+# RColorBrewer::brewer.pal(5, "Spectral")
+ngo_red <- "#D7191C"
+ngo_orange <- "#FDAE61"
+ngo_yellow <- "#FFFFBF"
+ngo_green <- "#ABDDA4"
+ngo_blue <- "#2B83BA"
+
+favorability_cols_5 <- data_frame(value = c("Very unfavorable", "Unfavorable", "Neutral", 
+                                            "Favorable", "Very favorable"),
+                                  color = c(ngo_red, ngo_orange, ngo_yellow,
+                                            ngo_green, ngo_blue))
+
+theme_ngos <- function(base_size = 11, base_family = "Encode Sans Condensed") {
+  update_geom_defaults("label", list(family = "Encode Sans Condensed Medium"))
+  update_geom_defaults("text", list(family = "Encode Sans Condensed Medium"))
+  
+  ret <- theme_bw(base_size, base_family) +
+    theme(plot.title = element_text(size = rel(1.4), face = "plain",
+                                    family = "Encode Sans Condensed SemiBold"),
+          plot.subtitle = element_text(size = rel(1), face = "plain",
+                                       family = "Encode Sans Condensed Light"),
+          plot.caption = element_text(size = rel(0.8), color = "grey50", face = "plain",
+                                      family = "Encode Sans Condensed Light"),
+          strip.text = element_text(size = rel(1), face = "plain",
+                                    family = "Encode Sans Condensed SemiBold"),
+          panel.border = element_blank(), 
+          axis.ticks = element_blank(),
+          strip.background = element_rect(fill = "#ffffff", colour = NA),
+          legend.title = element_text(size = rel(0.8)),
+          legend.position = "bottom")
+  ret
+}
---
+++ lib/pander_options.R
@@ -0,0 +1,7 @@
+panderOptions("table.split.table", Inf)
+panderOptions("table.split.cells", Inf)
+panderOptions("missing", "")
+panderOptions("big.mark", ",")
+panderOptions("digits", 3)
+panderOptions("round", 3)
+panderOptions("table.alignment.default", "left")
