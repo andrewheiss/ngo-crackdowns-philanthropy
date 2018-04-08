0e7e7f929d595516ba5df608c0ac319b91daa4ca
Andrew Heiss <andrewheiss@gmail.com>
2018-04-07 22:23:22 -0700

    Add simple model flag

--- lib/modeling.R
+++ lib/modeling.R
@@ -57,27 +57,27 @@ options(contrasts = rep("contr.treatment", 2))
 
 # Table of coefficients and their clean names
 clean_coefs <- tribble(
-  ~term, ~term_clean,
-  "(Intercept)", "Intercept",
-  "crackdownCrackdown", "Crackdown (yes)",
-  "issueHumanitarian assistance", "Issue (humanitarian)",
-  "fundingPrivate", "Funding (private)",
-  "crackdownCrackdown:issueHumanitarian assistance", "Crackdown × Issue",
-  "crackdownCrackdown:fundingPrivate", "Crackdown × Funding",
-  "issueHumanitarian assistance:fundingPrivate", "Issue × Funding",
-  "crackdownCrackdown:issueHumanitarian assistance:fundingPrivate", "Crackdown × Issue × Funding",
-  "favor_humanitarian_binFavorable", "Prior favorability towards humanitarian NGOs",
-  "favor_human_rights_binFavorable", "Prior favorability towards human rights NGOs",
-  "favor_development_binFavorable", "Prior favorability towards development NGOs",
-  "give_charity_3Once a month-once a year", "Give to charity once a month–once a year",
-  "give_charity_3At least once a month", "Give to charity at least once a month",
-  "volunteerYes", "Volunteered in past year",
-  "political_knowledge_binOften", "Follow current political evens often",
-  "ideology_binLiberal", "Liberal political views",
-  "education_binBA and above", "Bachelor's degree or higher",
-  "religiosity_binAt least once a month", "Attend religious services at least once a month",
-  "income_binAt least $50,000 – $59,999", "Income $50,000 or higher",
-  "age_binAt least 35 – 44", "Age 35 or higher"
+  ~simple_model, ~term, ~term_clean,
+  TRUE, "(Intercept)", "Intercept",
+  TRUE, "crackdownCrackdown", "Crackdown (yes)",
+  TRUE, "issueHumanitarian assistance", "Issue (humanitarian)",
+  TRUE, "fundingPrivate", "Funding (private)",
+  TRUE, "crackdownCrackdown:issueHumanitarian assistance", "Crackdown × Issue",
+  TRUE, "crackdownCrackdown:fundingPrivate", "Crackdown × Funding",
+  TRUE, "issueHumanitarian assistance:fundingPrivate", "Issue × Funding",
+  TRUE, "crackdownCrackdown:issueHumanitarian assistance:fundingPrivate", "Crackdown × Issue × Funding",
+  FALSE, "favor_humanitarian_binFavorable", "Prior favorability towards humanitarian NGOs",
+  # FALSE, "favor_human_rights_binFavorable", "Prior favorability towards human rights NGOs",
+  # FALSE, "favor_development_binFavorable", "Prior favorability towards development NGOs",
+  FALSE, "give_charity_3Once a month-once a year", "Give to charity once a month–once a year",
+  FALSE, "give_charity_3At least once a month", "Give to charity at least once a month",
+  FALSE, "volunteerYes", "Volunteered in past year",
+  FALSE, "political_knowledge_binOften", "Follow current political evens often",
+  FALSE, "ideology_binLiberal", "Liberal political views",
+  FALSE, "education_binBA and above", "Bachelor's degree or higher",
+  FALSE, "religiosity_binAt least once a month", "Attend religious services at least once a month",
+  FALSE, "income_binAt least $50,000 – $59,999", "Income $50,000 or higher",
+  FALSE, "age_binAt least 35 – 44", "Age 35 or higher"
 ) %>% 
   mutate(term_clean_fct = fct_inorder(term_clean, ordered = TRUE))
 
