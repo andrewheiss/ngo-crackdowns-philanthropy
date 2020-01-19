313a9fe967b0663b8aebd882484e502386358aca
Andrew Heiss <andrew@andrewheiss.com>
2020-01-19 00:26:21 -0500

    Improve summary_table functions
    They now get fed entire columns collapsed into a list instead of a bunch of character values that have to be reclassed here.

--- R/summary_table.R
+++ R/summary_table.R
@@ -5,38 +5,20 @@
 #' @name summary_table
 NULL
 
-my_as_numeric <- function(x) {
-  is_num <- suppressWarnings(!any(is.na(as.numeric(x))))
-  
-  if (is_num) {
-    as.numeric(x)
-  } else {
-    x
-  }
-}
-
-level_summary <- function(x, variable, df) {
-  variable <- as.character(variable)
-  if (is.factor(df[[variable]])) {
-    x_levels <- levels(df[[variable]])
-  } else {
-    x_levels <- unique(df[[variable]])
-  }
-  
-  factor_summary <- tibble(x) %>% 
-    count(x) %>%
+level_summary <- function(value, variable_name) {
+  factor_summary <- tibble(value) %>% 
+    count(value) %>%
     mutate(pct = n / sum(n),
-           fancy = paste0(x, " (", n, "; ", percent(pct), ")"),
-           x = factor(x, levels = x_levels, ordered = TRUE)) 
+           fancy = paste0(value, " (", n, "; ", percent(pct), ")"))
   
-  inline_summary <- factor_summary %>% arrange(x) %>% 
+  inline_summary <- factor_summary %>% arrange(value) %>% 
     pull(fancy) %>% paste(collapse = " | ")
   
-  fig_bar <- ggplot(factor_summary, aes(x = x, y = n)) +
+  fig_bar <- ggplot(factor_summary, aes(x = value, y = n)) +
     geom_bar(stat = "identity", fill = "grey50") +
     theme_spark()
   
-  file_name <- gsub("\\.| ", "_", tolower(variable))
+  file_name <- gsub("\\.| ", "_", tolower(variable_name))
   md_img <- paste0("![](output/figures/summary-table/", file_name, ")")
   
   ggsave(fig_bar, filename = here("analysis", "output", "figures", "summary-table",
@@ -50,16 +32,16 @@ level_summary <- function(x, variable, df) {
   return(output)
 }
 
-numeric_summary <- function(x, variable) {
-  inline_summary <- paste0("Median: ", round(median(x, na.rm = TRUE), 2),
-                           " | Mean: ", round(mean(x, na.rm = TRUE), 2), 
-                           " | Std. Dev.: ", round(sd(x, na.rm = TRUE), 2))
+numeric_summary <- function(value, variable_name) {
+  inline_summary <- paste0("Median: ", round(median(value, na.rm = TRUE), 2),
+                           " | Mean: ", round(mean(value, na.rm = TRUE), 2), 
+                           " | Std. Dev.: ", round(sd(value, na.rm = TRUE), 2))
   
-  fig_hist <- ggplot(as.data.frame(x), aes(x = x)) +
+  fig_hist <- ggplot(tibble(value), aes(x = value)) +
     geom_histogram(fill = "grey50", binwidth = 10) +
     theme_spark()
   
-  file_name <- gsub("\\.| ", "_", tolower(variable))
+  file_name <- gsub("\\.| ", "_", tolower(variable_name))
   md_img <- paste0("![](output/figures/summary-table/", file_name, ")")
   
   ggsave(fig_hist, filename = here("analysis", "output", "figures", "summary-table",
@@ -75,9 +57,8 @@ numeric_summary <- function(x, variable) {
 
 #' @rdname summary_table
 #' 
-#' @param x a one-column data frame
-#' @param variable name of the variable to summarize (generally the column name of x)
-#' @param df the data frame that x comes from
+#' @param value a numeric or factor vector
+#' @param variable_name name of the variable to summarize (generally the column name of value)
 #' 
 #' @export
 #' 
@@ -85,36 +66,36 @@ numeric_summary <- function(x, variable) {
 #' \dontrun{
 #' set.seed(1234)
 #' example_data <- tibble(numeric_thing = 1:100,
-#'                        factor_thing = factor(sample(LETTERS[1:4], 100, replace = TRUE)))
+#'                        factor_thing = factor(sample(LETTERS[1:4], 100, 
+#'                                                     replace = TRUE)))
 #' 
 #' # Single rows
-#' md_summary_row(example_data[[1]], "numeric_thing", example_data)
+#' md_summary_row(example_data[[1]], "numeric_thing")
 #' #> # A tibble: 1 x 2
-#' #>   spark                                           summary                                     
-#' #>   <chr>                                           <chr>                                       
+#' #>   spark                                           summary
+#' #>   <chr>                                           <chr>
 #' #> 1 ![](output/figures/summary-table/numeric_thing) Median: 50.5 | Mean: 50.5…
 #' 
 #' # Make a summary for all rows
 #' example_data %>% 
-#'   gather(variable, value) %>% 
-#'   group_by(variable) %>% 
-#'   nest() %>% 
-#'   mutate(summary = map2(.x = data, .y = variable, 
-#'                         ~ md_summary_row(.x$value, .y, example_data))) %>% 
-#'   select(-data) %>% 
+#'   summarise_all(~list(.)) %>% # Collapse each column into a nested cell
+#'   pivot_longer(everything(), names_to = "variable_name", values_to = "value") %>% 
+#'   mutate(summary = map2(.x = value, .y = variable_name, 
+#'                         ~ md_summary_row(.x, .y))) %>% 
+#'   select(-value) %>% 
 #'   unnest(summary)
 #' #> # A tibble: 2 x 3
-#' #>   variable      spark                                   summary             
-#' #>   <chr>         <chr>                                   <chr>               
+#' #>   variable      spark                                   summary
+#' #>   <chr>         <chr>                                   <chr>
 #' #> 1 numeric_thing ![](output/figures/summary-table/numer… Median: 50.5 | Mean…
-#' #> 2 factor_thing  ![](output/figures/summary-table/facto… A (31; 31.0%) | B (…
+#' #> 2 factor_thing  ![](output/figures/summary-table/facto… A (19; 19.0%) | B (…
 #' }
-md_summary_row <- function(x, variable, df) {
-  x <- my_as_numeric(x)
-  
-  if (is.numeric(x)) {
-    numeric_summary(x, variable)
+md_summary_row <- function(value, variable_name) {
+  if (is.numeric(value)) {
+    numeric_summary(value, variable_name)
+  } else if (is.factor(value)) {
+    level_summary(value, variable_name)
   } else {
-    level_summary(x, variable, df)
+    "This isn't numeric or a factor."
   }
 }
